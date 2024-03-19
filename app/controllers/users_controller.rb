bf5ab4306df95dabd310f79b007447790b37be2a
vincent marcelin <marcelinvincent@live.fr>
2024-03-19 16:51:40 +0100

    Controlleuruser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
   def show
-  
+  @user = User.find(params[:id])
+  @exploitations = @user.exploitations
+  @commandes = @exploitations.commandes
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Utilisateur mis à jour!'
+    else
+      flash.now[:alert] = 'Echec de la mise à jour'
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: 'Compte supprimé.'
+  end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/commande.rb
+++ app/models/commande.rb
@@ -1,4 +1,4 @@
 class Commande < ApplicationRecord
-  belongs_to :creneau
+  belongs_to :creneau, :commande
   has_many :produits
 end
