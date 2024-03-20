9f867c3414fbd8bfd1654b136dbc04f8cf9a60ab
camille caufour <culturecam@protonmail.com>
2024-03-20 18:16:54 +0100

    le fichier de controle des commandes

--- app/controllers/commandes_controller.rb
+++ app/controllers/commandes_controller.rb
@@ -1,2 +1,59 @@
+# app/controllers/commandes_controller.rb
 class CommandesController < ApplicationController
+  before_action :set_commande, only: [:show, :edit, :update, :destroy]
+
+  # GET /commandes toutes
+  def index
+    @commandes = Commande.all
+  end
+
+  # GET /commandes /show
+  def show
+  end
+
+  # GET /commandes/new
+  def new
+    @commande = Commande.new
+  end
+
+  # POST /commandes
+  def create
+    @commande = Commande.new(commande_params)
+
+    if @commande.save
+      redirect_to @commande, notice: 'Votre commande a été créée !'
+    else
+      render :new
+    end
+  end
+
+  # GET /commandes/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /commandes
+  def update
+    if @commande.update(commande_params)
+      redirect_to @commande, notice: 'Votre commande a été mise à jour'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /commandes
+  def destroy
+    @commande.destroy
+    redirect_to commandes_url, notice: 'Votre commande a bien été supprimée'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_commande
+      @commande = Commande.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def commande_params
+      params.require(:commande).permit(:creneau_id, :exploitation_id)
+    end
 end
