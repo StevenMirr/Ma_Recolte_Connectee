a2f5746ff19e727918bba94f77958a488670c998
vincent marcelin <marcelinvincent@live.fr>
2024-03-20 18:19:16 +0100

    exploitation + seeds

--- app/models/commande.rb
+++ app/models/commande.rb
@@ -1,5 +1,4 @@
 class Commande < ApplicationRecord
   belongs_to :creneau
-  belongs_to :commande
   has_many :produits
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,13 @@ Exploitation.destroy_all
 User.destroy_all
 
 user1 = User.create!(email: "superman@lewagon.com", password: "123456", name: "Superman")
+user2 = User.create!(email: "georges@lewagon.com", password: "123456", name: "Georges")
+user3 = User.create!(email: "fermier@lewagon.com", password: "123456", name: "Jean")
+user4 = User.create!(email: "jeanette@lewagon.com", password: "123456", name: "Jeanette")
+user5 = User.create!(email: "sophie@lewagon.com", password: "123456", name: "Sophie")
 
-exploitation1 = Exploitation.create!(name: 'Salade', address: "4 rue des choux", user: user1)
+exploitation1 = Exploitation.create!(name: 'Les Côteaux Nantais', address: "Les Ajoncs 3 pl Pierre Desfossés, Vertou", user: user1)
+exploitation2 = Exploitation.create!(name: 'Marmelade', address: "4 r Distillerie, Nantes", user: user2)
+exploitation3 = Exploitation.create!(name: "Graines d'ici", address: "71 bd Alfred Nobel, Rezé", user: user3)
+exploitation4 = Exploitation.create!(name: 'Moi, moche et bon', address: "93 r Libération, Saint Sébastien sur Loire", user: user4)
+exploitation5 = Exploitation.create!(name: "C'est qui le patron", address: "5 PL Alexandre Vincent, Nantes", user: user5)
