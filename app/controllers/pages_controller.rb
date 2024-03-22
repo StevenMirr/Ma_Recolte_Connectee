cc439197941ca2195bbf40f26fa13d50b625272a
Steven Miranda <steven.miranda@live.fr>
2024-03-22 12:53:51 +0100

    setup home page navbar et modif visuelle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @exploitations = Exploitation.all
+    @markers = @exploitations.geocoded.map do |exploitation|
+      {
+        lat: exploitation.latitude,
+        lng: exploitation.longitude,
+        info_window_html: render_to_string(partial: "exploitations/info_window", locals: { exploitation: exploitation }),
+        marker_html: render_to_string(partial: "exploitations/marker", locals: { exploitation: exploitation })
+      }
+    end
   end
 end
